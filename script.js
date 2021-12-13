12695ed9af56cc4e7b9299ddd418d3f8904bb375
Anderson <andeersonporteela@gmail.com>
2021-12-13 00:16:50 -0300

    Logical and style improvements

--- script.js
+++ script.js
@@ -2,22 +2,40 @@ const APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.de
 
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
-
+const main = document.querySelector('main');
 
 async function getMovies() {
     const resp = await fetch(APIURL);
     const respData = await resp.json();
 
     console.log(respData);
+    
+    respData.results.forEach((movie) => {
+        const {poster_path, title, vote_average} = movie;
 
-    respData.results.forEach(movie => {
-        const img = document.createElement('img');
-        img.src = IMGPATH + movie.poster_path;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+            <img src="${IMGPATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        `;
 
-        document.body.appendChild(img);
-    })
+        main.appendChild(movieEl);
+});
 
     return respData;
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green';
+    } else if (vote >= 6) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
 }
 
 getMovies();
